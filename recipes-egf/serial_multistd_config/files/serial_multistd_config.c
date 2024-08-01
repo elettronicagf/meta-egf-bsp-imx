95c68e1d50b6789af77918724baf1679cfe1b418
Andrea Collamati <andrea.collamati@elettronicagf.it>
2024-08-01 16:48:05 +0200

    Added serial_multistd_recipe.
    Tool to setup SP330EEY multiserial IC on EGF Boards

---
+++ recipes-egf/serial_multistd_config/files/serial_multistd_config.c
@@ -0,0 +1,215 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// derived from https://github.com/brgl/libgpiod/raw/master/examples/toggle_line_value.c
+// derived from https://gist.github.com/amarburg/07564916d8d32e20e6ae375c1c83a995
+// Tool to setup multiserial IC on EGF SOM
+
+#include <gpiod.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/serial.h>
+#include <sys/ioctl.h>
+#ifndef CONSUMER
+#define CONSUMER "Consumer"
+#endif
+struct gpiod_chip *chip2;
+struct gpiod_chip *chip4;
+struct gpiod_line *line_n_shdn;
+struct gpiod_line *line_echo_enable;
+struct gpiod_line *line_slew_rate;
+struct gpiod_line *line_rs485_enable;
+struct gpiod_line *line_half_duplex_enable;
+
+static struct gpiod_line_request *request_output_line(const char *chip_path, unsigned int offset,
+													  enum gpiod_line_value value, const char *consumer)
+{
+	struct gpiod_request_config *req_cfg = NULL;
+	struct gpiod_line_request *request = NULL;
+	struct gpiod_line_settings *settings;
+	struct gpiod_line_config *line_cfg;
+	struct gpiod_chip *chip;
+	int ret;
+
+	chip = gpiod_chip_open(chip_path);
+	if (!chip)
+		return NULL;
+
+	settings = gpiod_line_settings_new();
+	if (!settings)
+		goto close_chip;
+
+	gpiod_line_settings_set_direction(settings,
+									  GPIOD_LINE_DIRECTION_OUTPUT);
+	gpiod_line_settings_set_output_value(settings, value);
+
+	line_cfg = gpiod_line_config_new();
+	if (!line_cfg)
+		goto free_settings;
+
+	ret = gpiod_line_config_add_line_settings(line_cfg, &offset, 1,
+											  settings);
+	if (ret)
+		goto free_line_config;
+
+	if (consumer)
+	{
+		req_cfg = gpiod_request_config_new();
+		if (!req_cfg)
+			goto free_line_config;
+
+		gpiod_request_config_set_consumer(req_cfg, consumer);
+	}
+
+	request = gpiod_chip_request_lines(chip, req_cfg, line_cfg);
+
+	gpiod_request_config_free(req_cfg);
+
+free_line_config:
+	gpiod_line_config_free(line_cfg);
+
+free_settings:
+	gpiod_line_settings_free(settings);
+
+close_chip:
+	gpiod_chip_close(chip);
+
+	return request;
+}
+
+void enable_rs485(const char *port, int enable)
+{
+	int fd = open(port, O_RDWR);
+
+	if (fd < 0)
+	{
+		/* Error handling. See errno. */
+		fprintf(stderr, "Error opening port \"%s\" (%d): %s\n", port, errno, strerror(errno));
+		exit(-1);
+	}
+
+	struct serial_rs485 rs485conf;
+
+	if (ioctl(fd, TIOCGRS485, &rs485conf) < 0)
+	{
+		fprintf(stderr, "Error reading ioctl port (%d): %s\n", errno, strerror(errno));
+	}
+
+	printf("Port currently RS485 mode is %s\n", (rs485conf.flags & SER_RS485_ENABLED) ? "set" : "NOT set");
+
+	if (enable)
+	{
+
+		printf("RS485 mode will be SET\n");
+		rs485conf.flags |= SER_RS485_ENABLED | SER_RS485_RTS_ON_SEND;
+	}
+	else
+	{
+		printf("RS485 mode will be UNSET\n");
+		rs485conf.flags &= ~SER_RS485_ENABLED;
+	}
+
+	if (ioctl(fd, TIOCSRS485, &rs485conf) < 0)
+	{
+		fprintf(stderr, "Error sending ioctl port (%d): %s\n", errno, strerror(errno));
+	}
+
+	/* Use read() and write() syscalls here... */
+
+	if (ioctl(fd, TIOCGRS485, &rs485conf) < 0)
+	{
+		fprintf(stderr, "Error reading ioctl port (%d): %s\n", errno, strerror(errno));
+	}
+
+	printf("Confirm RS485 mode is %s\n", (rs485conf.flags & SER_RS485_ENABLED) ? "set" : "NOT set");
+}
+
+int set_output(const char *chip_path, int line_offset, int value)
+{
+
+	struct gpiod_line_request *request;
+	printf("%s %d => %d\n",chip_path, line_offset, value);
+
+	request = request_output_line(chip_path, line_offset, (enum gpiod_line_value)value,
+								  "toggle-line-value");
+	if (!request)
+	{
+		fprintf(stderr, "failed to request line: %s\n",
+				strerror(errno));
+		exit(-1);
+	}
+
+	gpiod_line_request_set_value(request, line_offset, (enum gpiod_line_value) value);
+
+	gpiod_line_request_release(request);
+
+	return 0;
+}
+
+void print_usage()
+{
+#if WSM0880
+	printf("serial_multistd_config [off/rs232/rs485hd/rs485fd]\nuart /dev/ttymxc1 on connector CN15");
+#elif WSM0890
+	printf("serial_multistd_config [off/rs232/rs485hd/rs485fd]\nuart /dev/ttymxc0 on connector CN33");
+#endif
+}
+
+void set_io(int n_shdn, int echo, int half_duplex, int rs485_en, int slew)
+{
+#if WSM0880
+	set_output("/dev/gpiochip4", 1, echo);
+	set_output("/dev/gpiochip2", 20, slew);
+	set_output("/dev/gpiochip2", 24, n_shdn);
+	set_output("/dev/gpiochip4", 27, rs485_en);
+	set_output("/dev/gpiochip4", 26, half_duplex);
+	enable_rs485("/dev/ttymxc1",rs485_en);
+#elif WSM0890
+#define GPIO_EXPANDER_U77	"/dev/gpiochip7"
+	set_output(GPIO_EXPANDER_U77, 7, echo); 		// P0_7
+	set_output(GPIO_EXPANDER_U77, 15, slew);		// P1_7
+	set_output(GPIO_EXPANDER_U77, 14, n_shdn);		// P1_6
+	set_output(GPIO_EXPANDER_U77, 13, rs485_en);    // P1_5
+	set_output(GPIO_EXPANDER_U77, 12, half_duplex); // P1_4
+	enable_rs485("/dev/ttymxc0",rs485_en);
+#else
+#error "undefined Board model"
+#endif
+}
+
+int main(int argc, char **argv)
+{
+
+	argc--;
+	if (argc != 1)
+	{
+		print_usage();
+		exit(-1);
+	}
+	char *par = argv[1];
+	if (!strcmp(par, "off"))
+	{
+		printf("Switching Off Transceiver\n");
+		set_io(0, 0, 0, 0, 0);
+	}
+	else if (!strcmp(par, "rs232"))
+	{
+		set_io(1, 0, 1, 0, 0);
+	}
+	else if (!strcmp(par, "rs485hd"))
+	{
+		set_io(1, 0, 1, 1, 0);
+	}
+	else if (!strcmp(par, "rs485fd"))
+	{
+		set_io(1, 0, 0, 1, 0);
+	}
+	else
+	{
+		print_usage();
+		return -1;
+	}
+	return 0;
+}
---
+++ recipes-egf/serial_multistd_config/serial-multistd-config.bb
@@ -0,0 +1,25 @@
+
+DESCRIPTION = " tool to setup SP330EEY multiserial IC on EGF Boards"
+
+DEPENDS = " libgpiod "
+SRC_URI = "file://serial_multistd_config.c"
+LICENSE = "GPL-2.0-or-later"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0-or-later;md5=fed54355545ffd980b814dab4a3b312c"
+
+
+BOARDCONFIG:imx8mp-egf-3sm1009  = "-DWSM0890=1"
+BOARDCONFIG:imx8mm-egf-3sm1008m = "-DWSM0880=1"
+
+
+do_compile() {
+    ${CC} ${BOARDCONFIG}  -Wl,--hash-style=gnu -o  ${WORKDIR}/serial_multistd_config -lgpiod ${WORKDIR}/serial_multistd_config.c
+}
+
+
+
+do_install() {
+    install -d ${D}${sbindir}/
+    install -m 0755 ${WORKDIR}/serial_multistd_config ${D}${sbindir}/serial_multistd_config
+}
+
+PACKAGE_ARCH = "${MACHINE_ARCH}"
